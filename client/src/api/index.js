ef0e7f4be21014e721afc491076a5919f4ce6c47
Mohamed Rizwan <140683620+MOHAMEDRIZWAN07@users.noreply.github.com>
2024-04-27 20:57:31 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const API = axios.create({ baseURL: `http://localhost:5500/` });
- const API = axios.create({ baseURL: `https://studytube-cq2r.onrender.com/` });
+ const API = axios.create({ baseURL: `https://study-tube-zrkh.onrender.com/`});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
